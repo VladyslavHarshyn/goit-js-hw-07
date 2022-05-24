4d8e78367b6301f34191e843ca855b72d8a81681
Vladyslav Harshyn <garshyn_98@ukr.net>
2022-05-24 22:23:25 +0300

    fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
+'use strict';
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+function addItem() {
+    return galleryItems.map(items => 
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${items.original}">
+    <img
+        class="gallery__image"
+        src="${items.preview}"
+        data-source="${items.original}"
+        alt="${items.description}"
+    />
+    </a>
+</div>`
+    )
+    .join('')
+}
+
+gallery.insertAdjacentHTML('afterbegin', addItem(galleryItems))
+
 console.log(galleryItems);
+
+let instance;
+
+gallery.addEventListener("click", galleryClick);
+
+function galleryClick(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+
+    instance.show();
+  
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+"use strict";
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+function addItem() {
+  return galleryItems
+    .map(
+      (items) =>
+        `<a class="gallery__item" href="${items.original}">
+  <img class="gallery__image" src="${items.preview}" alt="${items.description}" />
+</a>`
+    )
+    .join("");
+}
+
+gallery.insertAdjacentHTML("afterbegin", addItem(galleryItems));
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
